b6bf021a82fabb0edbcf17fc0457eeebcf252809
Radim Tesner <radim.tesner.022@pslib.cz>
2024-04-03 15:40:35 +0200

    hahahah

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
 function toggleMenu() {
-    var menuItems = document.getElementById("menu-items");
-    if (menuItems.style.display === "none" || menuItems.style.display === "") {
-        menuItems.style.display = "block";
-        document.documentElement.style.background = "linear-gradient(to bottom, rgba(91, 37, 180, 1), rgba(66, 91, 169, 1), rgba(44, 138, 159, 1))";
-        document.documentElement.style.backgroundAttachment = "fixed";
-    } else {
-        menuItems.style.display = "none";
-        document.documentElement.style.background = "black";
-    }
+    var menuNavbar = document.querySelector('.menu-items');
+    menuNavbar.classList.toggle('active');
 }
