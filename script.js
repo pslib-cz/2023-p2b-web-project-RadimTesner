c6afe86060041ccf1e6f08dd08559c3ad735c849
Radim Tesner <radim.tesner.022@pslib.cz>
2024-04-03 21:39:19 +0200

    21:39 index hotvov umiram toto je zacatek meho spadu into madness save me...

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ function toggleMenu() {
     var menuNavbar = document.querySelector('.menu-items');
     menuNavbar.classList.toggle('active');
 }
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 'auto',
+    spaceBetween: 30,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
