b503db1fa9db591ab55bd4186ca4859df4eca3d0
Radim Tesner <radim.tesner.022@pslib.cz>
2024-04-04 18:16:59 +0200

    mobile finished

--- script.js
+++ script.js
@@ -2,11 +2,9 @@ function toggleMenu() {
     var menuNavbar = document.querySelector('.menu--items');
     menuNavbar.classList.toggle('active');
 }
-var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 'auto',
-    spaceBetween: 30,
+var swiper = new Swiper(".swiper", {
     pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
+      el: ".swiper-pagination",
+      dynamicBullets: true,
     },
 });
